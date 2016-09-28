c5b086f1cca5b8685a634e6fc5f8c880dc1869e8
spiritphyz <heytonyle@icloud.com>
2016-09-28 16:27:31 -0700

    Implement model functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -14,14 +14,24 @@ var Movies = Backbone.Collection.extend({
 
   model: Movie,
 
-  initialize: function() {
-    // your code here
+  initialize: function(data) {
+    // console.log(this);
+    // var model;
+    // var collection = this;
+    // _.each(data, function(datum) {
+    //   console.log(this);
+    //   model = new Movie(datum);
+    //   model.on('change:like', collection.sort);
+    //   console.log(model);
+    //   this.models.push(model);
+    // }.bind(this));
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
